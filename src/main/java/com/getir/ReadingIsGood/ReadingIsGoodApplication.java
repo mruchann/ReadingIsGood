9d7d54934343f231ef496f3cf9c9e1b231551331
ruchan <ruchan.yavuzdemir@icloud.com>
2023-02-13 02:01:50 +0300

    Add annotations

--- src/main/java/com/getir/ReadingIsGood/ReadingIsGoodApplication.java
+++ src/main/java/com/getir/ReadingIsGood/ReadingIsGoodApplication.java
@@ -2,14 +2,21 @@ package com.getir.ReadingIsGood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class ReadingIsGoodApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadingIsGoodApplication.class, args);
 	}
-
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
-
-// TODO: Git, Docker-PostgreSQL
