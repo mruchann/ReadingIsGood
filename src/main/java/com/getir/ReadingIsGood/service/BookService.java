df055ac2905e79ee11ba5bf8f196054615303f2b
ruchan <ruchan.yavuzdemir@icloud.com>
2023-02-21 23:24:52 +0300

    Create services

---
+++ src/main/java/com/getir/ReadingIsGood/service/BookService.java
@@ -0,0 +1,7 @@
+package com.getir.ReadingIsGood.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+}
---
+++ src/main/java/com/getir/ReadingIsGood/service/CustomerService.java
@@ -0,0 +1,16 @@
+package com.getir.ReadingIsGood.service;
+
+import com.getir.ReadingIsGood.model.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    public List<Order> listCustomerOrdersById(Integer id) {
+
+    }
+
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+    }
+}
