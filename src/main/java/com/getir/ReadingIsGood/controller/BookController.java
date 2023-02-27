4ed65c8e433fb5933cec48aa4b6e5fecec9f94b6
ruchan <ruchan.yavuzdemir@icloud.com>
2023-02-27 23:24:46 +0300

    Update services and controllers

--- src/main/java/com/getir/ReadingIsGood/controller/BookController.java
+++ src/main/java/com/getir/ReadingIsGood/controller/BookController.java
@@ -1,28 +1,28 @@
 package com.getir.ReadingIsGood.controller;
 
 import com.getir.ReadingIsGood.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "api/v1/book")
+@RequestMapping(path = "api/v1/books")
 public class BookController {
 
     private final BookService bookService;
-
+    @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-    /*
     @PostMapping
     public void addNewBook(@RequestBody BookAddingRequest bookAddingRequest) {
-        return bookService.addNewBook(bookAddingRequest);
+        return bookService.addBook(bookAddingRequest);
     }
 
     @PutMapping(value = "/{id}")
     public void updateBookStock(@PathVariable("id") Integer id) {
+
         return bookService.updateBookStock(id);
     }
-     */
 }
 
 // ResponseEntity<?>
