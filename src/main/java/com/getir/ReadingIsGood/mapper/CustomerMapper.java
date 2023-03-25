481d86fbdfc034272595ea70538cc26be1ccfbf8
ruchan <ruchan.yavuzdemir@icloud.com>
2023-03-25 07:03:47 +0300

    Update mappers

--- src/main/java/com/getir/ReadingIsGood/mapper/CustomerMapper.java
+++ src/main/java/com/getir/ReadingIsGood/mapper/CustomerMapper.java
@@ -2,16 +2,10 @@ package com.getir.ReadingIsGood.mapper;
 
 import com.getir.ReadingIsGood.entity.Customer;
 import com.getir.ReadingIsGood.response.CustomerResponse;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface CustomerMapper {
-    @Mappings({
-            @Mapping(source = "orders", target = "customer.orders"),
-            @Mapping(source = "firstName", target = "customer.firstName"),
-            @Mapping(source = "lastName", target = "customer.lastName"),
-            @Mapping(source = "email", target = "customer.email"),
-            @Mapping(source = "password", target = "customer.password")
-    })
+
     CustomerResponse toCustomerResponse(Customer customer);
 }
--- src/main/java/com/getir/ReadingIsGood/mapper/OrderMapper.java
+++ src/main/java/com/getir/ReadingIsGood/mapper/OrderMapper.java
@@ -3,17 +3,10 @@ package com.getir.ReadingIsGood.mapper;
 import com.getir.ReadingIsGood.entity.Order;
 import com.getir.ReadingIsGood.response.OrderResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mappings;
-import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
-    @Mappings({
-            @Mapping(source = "customer", target = "orderResponse.customer"),
-            @Mapping(source = "books", target = "orderResponse.books"),
-            @Mapping(source = "totalPrice", target = "orderResponse.totalPrice"),
-            @Mapping(source = "orderDate", target = "orderResponse.orderDate")
-    })
+
     OrderResponse toOrderResponse(Order order);
 }
 
